036362ddebe1890861921b8bc7e4d35ccb9fa4cc
SelwinS <selwinsam@icloud.com>
2021-04-14 13:31:55 +0530

    third commit

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,17 @@
 from django.db import models
 
+
 class Blog(models.Model):
     title = models.CharField(max_length = 255)
     pubdate = models.DateTimeField()
     body = models.TextField()
     image = models.ImageField(upload_to = 'images/')
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:75]
+
+    def format_time(self):
+        return self.pubdate.strftime('%b %d, %Y')
